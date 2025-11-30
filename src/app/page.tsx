a676a3b5974f4543dd1e9b36b9b1cffd29401cfe
bastndev <gohit21@gmail.com>
2025-11-29 23:05:46 -0500

    🏗️ [Update] dark mode background, add a new blue div, remove Heart icon, and refactor Moon icon styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { Heart, Moon, Sun } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-red-500 sm:items-start">
+        <div className="bg-blue-500 w-full h-12"></div>
         <h1>Hello world</h1>
-          <Heart size={24} strokeWidth={2} className="text-[#ff714d]" />
-          <Moon size={24} strokeWidth={2} color="#ff4d6d" />
-          <Sun size={24} strokeWidth={2} color="#ff4d6d" />
+        <Moon className="h-6 w-6 stroke-2 text-[#ff4d6d]" />
+        <Sun size={24} strokeWidth={2} color="#ff4d6d" />
       </main>
     </div>
   );
