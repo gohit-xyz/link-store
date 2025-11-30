4deef5a7e68535658258aeb2cc30c8a203419ec8
bastndev <gohit21@gmail.com>
2025-11-29 23:42:04 -0500

    🏗️ [add] dark mode support with a new theme toggle component and `next-themes` integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { Heart, Moon, Sun } from "lucide-react";
 import { Button } from "../components/ui/button";
+import { ThemeToggle } from "../components/theme-toggle.";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-red-500 sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="bg-blue-500 w-full h-12"></div>
         <h1>Hello world</h1>
         <Moon className="h-6 w-6 stroke-2 text-[#ff4d6d]" />
         <Sun size={24} strokeWidth={2} color="#ff4d6d" />
         <Button className="cursor-pointer">Button</Button>
+        <ThemeToggle />
       </main>
     </div>
   );
