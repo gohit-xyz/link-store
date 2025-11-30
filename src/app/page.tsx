bb2c7e36bdcf8b75a5f5357e9c30bcffa1f4c9af
bastndev <gohit21@gmail.com>
2025-11-30 00:02:58 -0500

    🏗️ [refactor] update page layout and correct theme toggle component filename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { ThemeToggle } from "../components/theme-toggle.";
+import { ThemeToggle } from "../components/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="bg-blue-500 w-full h-50"></div>
+    <div className="min-h-screen w-full bg-zinc-50 font-sans dark:bg-black">
+      <header className="flex w-full items-center justify-end p-4">
         <ThemeToggle />
+      </header>
+      <main className="bg-amber-500 container mx-auto flex flex-col items-center p-4">
+        {/* Ready for content */}
       </main>
     </div>
   );
